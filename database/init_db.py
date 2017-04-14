4b399c3da1c5d8da1fe104af0028d3e258f2958b
Aditya <adityadivekar03@gmail.com>
2017-04-15 02:16:24 +0530

    Set up SQL tables. Other minor changes.

--- database/init_db.py
+++ database/init_db.py
@@ -3,10 +3,12 @@
 #!python3
 import os
 import json
-from pymongo import MongoClient
+from pymongo import *
 from py2neo import *
+from sqlalchemy import *
+import _mysql
 
-class videoinfo:
+class Videoinfo(object):
 	""" Mongodb DB of video's raw info. """
 
 	def init_db(self):
@@ -17,7 +19,7 @@ class videoinfo:
 		"""
 		client = MongoClient()
 		db = client.YourTube
-		path = os.getcwd() + '/database_api/video_info/'
+		path = os.getcwd() + '/database/video_info/'
 
 		try:
 			db.video_info.delete_many({})
@@ -57,40 +59,54 @@ class videoinfo:
 
 		# Hash index is created for `id`
 		db.video_info.create_index([("videoInfo.id", HASHED)], name="id");
-		
-		print("Video Info Database Set")
+		print("Video Info Database Set.")
 
-
-class videorel:
+class Videorel(object):
 	""" Neo4j database to store the relation between videos.
 		Relation has a attribute called weight. """
 
+	def __init__(self):
+		authenticate("localhost:7474", "YourTube", "pass")
+		self.graph = Graph("http://localhost:7474/db/data/");
+
 	def init_db(self):
 		""" Create and populate the Neo4j database `video`. """
-
-		files = os.listdir(os.getcwd() + '/database_api/video_info/');
-
+		print('Setting up Neo4j database. This will take some time.')
+		files = os.listdir(os.getcwd() + '/database/video_info/')
 		authenticate("localhost:7474", "YourTube", "pass")
-		graph = Graph();
+		graph = Graph("http://localhost:7474/db/data/");
 
 		data = [];
 
 		for f in files:
-			data_file = open(os.getcwd() + '/database_api/video_info/' + f);
+			data_file = open(os.getcwd() + '/database/video_info/' + f);
 			data.append(json.load(data_file));
 			data_file.close();
 
-		graph.run("MATCH (v) DETACH DELETE v");
+		self.graph.run("MATCH (v) DETACH DELETE v");
 		ids = [];
 		data;
 		for i in range(0,len(data)):
 			d = data[i];
-			rv = graph.run("CREATE (v:video { vid:" + str(d['videoInfo']['id']) + ", commentCount:" + str(d['videoInfo']['statistics']['commentCount']) + ", viewCount:" + str(d['videoInfo']['statistics']['viewCount']) + ", favoriteCount:" +  str(d['videoInfo']['statistics']['favoriteCount']) + ", dislikeCount:" + str(d['videoInfo']['statistics']['dislikeCount']) + ", likeCount:" + d['videoInfo']['statistics']['likeCount'] + "}) RETURN ID(v);" );
+			rv = self.graph.run(
+				"""
+				CREATE (v:video 
+				{ vid: '%s', commentCount: %s,
+				viewCount: %s, favoriteCount: %s,
+				dislikeCount: %s, likeCount: %s})
+				RETURN ID(v);
+				"""
+				 % (str(d['videoInfo']['id']),
+				str(d['videoInfo']['statistics']['commentCount']),
+				str(d['videoInfo']['statistics']['viewCount']),
+				str(d['videoInfo']['statistics']['favoriteCount']),
+				str(d['videoInfo']['statistics']['dislikeCount']),
+				str(d['videoInfo']['statistics']['likeCount'])) )
 			x = 12;
 			for record in rv:
 				x = record;
 			ids.append(x[u'ID(v)']);
-			print i;
+			# print(i);
 
 
 		for i in range(0,len(data)-1):
@@ -129,5 +145,37 @@ class videorel:
 					weightage = 100*(same_channel + percentage_desc_match + 2*percentage_tag_match);
 
 					if (weightage > 0):
-						graph.run("MATCH (v1:video), (v2:video) WHERE ID(v1) = " + str(ids[i]) + " AND ID(v2) = " + str(ids[j]) + " CREATE (v1)-[r2:WEIGHT {weight:" + str(weightage) + "}]->(v2);");
-		print("Relation between vidoes created.")
+						self.graph.run(
+							"""
+							MATCH (v1:video), (v2:video)
+							WHERE ID(v1) = %s AND ID(v2) = %s
+							CREATE (v1)-[r2:WEIGHT {weight: %s}]->(v2);
+							""" % (str(ids[i]), str(ids[j]), str(weightage)) )
+		print("Relation between videos created.")
+
+class UserDB(object):
+	""" Setup the needed SQL tables. """
+	def __init__(self):
+		# requires a 'user' database in which the tables are created.
+		engine = create_engine(
+			"mysql+pymysql://root:pass@localhost/user?host=localhost?port=3306")
+		engine.echo = True
+		conn = engine.connect()
+		metadata = MetaData(engine)
+		# metadata = MetaData()
+		userinfo = Table('userinfo', metadata,
+			Column('user_id', Integer),
+			Column('videoid', String(30)),
+			Column('latest_timestamp', String(30)),
+			Column('viewCount', Integer),
+			Column('likes', Integer),
+			Column('dislikes', Integer),
+			UniqueConstraint('user_id', 'videoid')
+			)
+
+		usercred = Table('usercred', metadata,
+			Column('user_id', Integer, primary_key=True),
+			Column('password', String(30)),
+			)
+		metadata.create_all(engine)
+		print('SQL tables set up.')
