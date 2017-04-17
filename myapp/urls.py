958b3afc99e11eef855b508929ce7babb414ae8a
Amogh Shankar Gupta <amogh14@gmail.com>
2017-04-18 01:32:00 +0530

    Initialize bootstrap

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^test/', views.test_func, name='test'),
+    url(r'^bootstrap/', views.bootstrap, name='boot'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -14,3 +14,6 @@ def test_func(request):
 		s += d['videoInfo']['snippet']['title']
 		s += '$$'
 	return HttpResponse("%s." % s)
+
+def bootstrap(request):
+	return render(request, 'myapp/bootstrap.html')	
