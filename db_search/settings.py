c94b19cc1ba808bdfe54b20cdceed6e8c7731452
Aditya <adityadivekar03@gmail.com>
2017-04-19 18:37:12 +0530

    Misc.

--- db_search/settings.py
+++ db_search/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from database.api import *
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
@@ -16,4 +17,29 @@ def test_func(request):
 	return HttpResponse("%s." % s)
 
 def bootstrap(request):
-	return render(request, 'myapp/bootstrap.html')	
+	return render(request, 'myapp/bootstrap.html')
+
+def login_view(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			return render(request, 'home.html', {'firstname':user.first_name,
+				'lastname':user.last_name, 'username':user.username})
+		else:
+			error = "Your account has been disabled. Use a different account. "
+			return render (request, 'login.html', {'error':error})
+	else:
+		error = "Incorrect username or password provided."
+		return render (request, 'login.html', {'error':error})
+
+# Isn't really used. Using django.contrib.auth.views.logout instead.
+def logout_view(request):
+	logout(request)
+	return redirect('/login/')
+
+def on_search_click(request):
+	# Will update userinfo table, historytags.
+	
