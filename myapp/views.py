1f3e1fab45126e43cd2e77e6417463fbc3821728
Mehul Jain <mehul.jain2029@gmail.com>
2017-04-20 01:28:49 +0530

    ignore sql

--- myapp/views.py
+++ myapp/views.py
@@ -100,23 +100,6 @@ def login_view(request):
 			error = 'Something went wrong! Please try again.'
 			return render(request, 'myapp/login.html', { 'error': 'Invalid username' })
 
-# def login_view(request):
-# 	username = request.POST['username']
-# 	password = request.POST['password']
-# 	user = authen(user=username, password=password)
-# 	if user is not None:
-# 		if user.is_active:
-# 			login(request, user)
-# 			return render(request, 'home.html', {'firstname':user.first_name,
-# 				'lastname':user.last_name, 'username':user.username})
-# 		else:
-# 			error = "Your account has been disabled. Use a different account. "
-# 			return render (request, 'login.html', {'error':error})
-# 	else:
-# 		error = "Incorrect username or password provided."
-# 		return render (request, 'login.html', {'error':error})
-
-# Isn't really used. Using django.contrib.auth.views.logout instead.
 def logout_view(request):
 	logout(request)
 	return redirect('/login/')
